75f07feff658a353d88879c015743bd78cd59ae5
7kQon <kanggakky1214@gmail.com>
2019-03-08 20:05:25 +0900

    add Node.js SocketIO Plugin, Jump-Pad realized
    Node.js SocketIO 플러그인 Asset\Plugins 에 넣었고 점프패드 구현함
    Notice)
    특정상황에서 점프패드를 밟고 점프 후 공중에서 좌/우 입력이 안되는 문제가 있음

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,22 +8,19 @@ public class Player : MonoBehaviour {
 
     public float m_hp;
     public float m_moveSpeed;
+    public float m_jumpForce;
     private float m_maxSpeed;
-    public bool isMovable;
+    public bool isMovable; //Player의 이동가능여부 플래그
     public Vector3 m_velocity;
-    //public Weapon weapon;
+    //public Weapon weapon; 
 
-    //movement 브랜치 테스트 1
-    //movement 브랜치 테스트 1
-    //movement 브랜치 테스트 222
-    //깃 테스트 12412412
-    
 	// Use this for initialization
 	void Start () {
         m_tr = GetComponent<Transform>();
         m_rb = GetComponent<Rigidbody2D>();
         m_hp = 3.0f;
         m_moveSpeed = 15.0f;
+        m_jumpForce = 600.0f;
         m_maxSpeed = 6.0f;
         isMovable = true;
 	}
@@ -36,28 +33,48 @@ public class Player : MonoBehaviour {
 	}
 
     public void die() {
-
+        // 리스폰 장소에서 다시 리스폰
     }
 
     public void move() {
         if (!isMovable)
+        {
+            Debug.Log("return");
             return;
+        }
+            
 
         if (Mathf.Abs(m_rb.velocity.x) > m_maxSpeed)
         {
-
+            Debug.Log("blank");
+            /*if (m_rb.velocity.x > 0)
+            {
+                m_rb.velocity = new Vector2(5.9f, 0f);
+            }
+            else if (m_rb.velocity.x < 0) {
+                m_rb.velocity = new Vector2(-5.9f, 0f);
+            }*/
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-            m_rb.AddForce(Vector3.right * m_moveSpeed);
-            m_tr.localScale = new Vector3(1.5f,1.5f, 0f);
+            Debug.Log("right");
+            m_rb.AddForce(Vector3.right * m_moveSpeed);    //AddForce는 Time.deltaTime을 곱해줄 필요가 없다
+            m_tr.localScale = new Vector3(1.5f,1.5f,0f);   //localScale을 좌우로 바꾼다
         }
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
+            Debug.Log("left");
             m_rb.AddForce(Vector3.left * m_moveSpeed);
-            m_tr.localScale = new Vector3(-1.5f, 1.5f, 0f);
+            m_tr.localScale = new Vector3(-1.5f,1.5f,0f);
         }
-        else {}
+        else {
+            Debug.Log("else");
+        }
+    }
+
+    public void jump() {
+        Debug.Log("Jump!");
+        m_rb.AddForce(Vector3.up * m_jumpForce);
     }
 
     public float getDamage(float currentHp, float damage) {
