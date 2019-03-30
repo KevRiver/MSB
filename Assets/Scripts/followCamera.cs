a99464f574e5ac407fd2ff6ab57eb673f20f8a69
moonjuhan <codemike@naver.com>
2019-03-30 14:50:48 +0900

    feat - 카메라가 플레이어 따라가는 기능 추가

---
+++ Assets/Scripts/followCamera.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followCamera : MonoBehaviour
+{
+    public Transform target;
+    public float dist = 10.0f;
+    public float height = 5.0f;
+    public float smoothRotate = 5.0f;
+
+    private Transform tr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+
+    void LateUpdate()
+    {
+        float currYAngle = Mathf.LerpAngle(tr.eulerAngles.y, target.eulerAngles.y, smoothRotate * Time.deltaTime);
+
+        Quaternion rot = Quaternion.Euler(0, currYAngle, 0);
+
+        tr.position = target.position - (rot * Vector3.forward * dist) + (Vector3.up * height);
+
+        tr.LookAt(target);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
