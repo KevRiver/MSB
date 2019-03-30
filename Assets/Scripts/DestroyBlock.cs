092f4c4e7b938eca5358820991771b2b1e15453d
moonjuhan <codemike@naver.com>
2019-03-30 16:19:32 +0900

    fix - 타일맵에서 걸리는 현상 수정

---
+++ Assets/Scripts/DestroyBlock.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBlock : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("space");
+            Destroy(gameObject);
+        }
+    }
+}
