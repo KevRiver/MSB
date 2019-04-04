74e15c06fd13c7bfd5bc0067eb31793c3ce87ac1
moonjuhan <codemike@naver.com>
2019-04-04 21:17:31 +0900

    feat - Gon, MJ 프로젝트 합치기

---
+++ Assets/Attack.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Hit !");
+        }
+    }
+}
---
+++ Assets/Scripts/NetworkModule.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+
+public class NetworkModule : MonoBehaviour
+{
+    public GameObject socketManager;
+
+    private SocketIOComponent m_SocketIOComponet;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        m_SocketIOComponet = socketManager.GetComponent<SocketIOComponent>();
+        m_SocketIOComponet.On("open", onSocketOpen);
+        m_SocketIOComponet.On("error", onSocketError);
+        m_SocketIOComponet.On("close", onSocketClose);
+
+    }
+
+    public SocketIOComponent get_socket()
+    {
+        if (m_SocketIOComponet == null)
+        {
+            Debug.Log("socket is null");
+        }
+        return m_SocketIOComponet;
+    }
+
+    private void onSocketOpen(SocketIOEvent e)
+    {
+        Debug.Log("socket opened" + e);
+    }
+
+    private void onSocketError(SocketIOEvent e)
+    {
+        Debug.Log("socket error" + e);
+    }
+
+    private void onSocketClose(SocketIOEvent e)
+    {
+        Debug.Log("socket closed" + e);
+    }
+}
