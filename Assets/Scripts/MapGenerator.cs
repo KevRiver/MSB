c16eb8eead2ddee489afe7eedaa5f60fa64956c4
moonjuhan <codemike@naver.com>
2019-04-04 21:42:01 +0900

    fix - 카메라와 캔버스 오류 수정

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -106,23 +106,23 @@ public class MapGenerator : MonoBehaviour
                 switch(mapStruct.mapArray[x, y])
                 {
                     case '1':
-                        Instantiate(blockGray_12, new Vector3(x - (mapStruct.mapWidth / 2), mapStruct.mapHeight - 5.5f - y, 100), Quaternion.identity);
+                        Instantiate(blockGray_12, new Vector3(x - (mapStruct.mapWidth / 2), mapStruct.mapHeight - 5.5f - y, 0), Quaternion.identity);
                         break;
                     case '2':
-                        Instantiate(blockGray_11, new Vector3(x - (mapStruct.mapWidth / 2), mapStruct.mapHeight - 5.5f - y, 100), Quaternion.identity);
+                        Instantiate(blockGray_11, new Vector3(x - (mapStruct.mapWidth / 2), mapStruct.mapHeight - 5.5f - y, 0), Quaternion.identity);
                         break;
                     case '3':
-                        Instantiate(blockBrown_12, new Vector3(x - (mapStruct.mapWidth / 2), mapStruct.mapHeight - 5.5f - y, 100), Quaternion.identity);
+                        Instantiate(blockBrown_12, new Vector3(x - (mapStruct.mapWidth / 2), mapStruct.mapHeight - 5.5f - y, 0), Quaternion.identity);
                         break;
                     case '4':
-                        Instantiate(blockBrown_11, new Vector3(x - (mapStruct.mapWidth / 2), mapStruct.mapHeight - 5.5f - y, 100), Quaternion.identity);
+                        Instantiate(blockBrown_11, new Vector3(x - (mapStruct.mapWidth / 2), mapStruct.mapHeight - 5.5f - y, 0), Quaternion.identity);
                         break;
                 }
 
                 switch (mapStruct.objectArray[x, y])
                 {
                     case '2':
-                        Instantiate(jumpPad, new Vector3(x - (mapStruct.mapWidth / 2), mapStruct.mapHeight - 5.5f - y, 100), Quaternion.identity);
+                        Instantiate(jumpPad, new Vector3(x - (mapStruct.mapWidth / 2), mapStruct.mapHeight - 5.5f - y, 0), Quaternion.identity);
                         break;
                 }
 
