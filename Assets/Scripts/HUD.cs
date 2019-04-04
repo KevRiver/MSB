0680eed876a560d36a91355fd2fe5e5144dfd617
7kQon <kanggakky1214@gmail.com>
2019-04-04 19:52:32 +0900

    190404
    근접 공격
    소켓 IO

---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    public Sprite[] heartSprites;
+    public Image heartUI;
+
+    private Player player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    private void Update()
+    {
+        heartUI.sprite = heartSprites[player.m_hp];
+    }
+}
