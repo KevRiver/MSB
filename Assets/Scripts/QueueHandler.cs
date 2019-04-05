fdc959093a4a5248bf3702e6aab58221108dc5f6
7kQon <kanggakky1214@gmail.com>
2019-04-05 15:14:26 +0900

    solo match make
    영어쓰게 존나 불편하니까 걍 한국어로 ㄱ
    정진영한테 개 꼽먹으면서 플레이어 큐에 참가까지 테스트 해봄

---
+++ Assets/Scripts/QueueHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+
+public class QueueHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnQueueButtonClick()
+    {
+        GameObject networkmodule = GameObject.Find("NetworkModule");
+        SocketIOComponent socket;
+        JSONObject userData = new JSONObject(JSONObject.Type.OBJECT);
+
+        socket = networkmodule.GetComponent<NetworkModule>().get_socket();
+        socket.On("soloMatched",soloMatched);
+        socket.On("soloRoomFull",soloRoomFull);
+        socket.Emit("matchMakeSolo");
+    }
+
+    public void soloMatched(SocketIOEvent e)
+    {
+
+    }
+
+    public void soloRoomFull(SocketIOEvent e)
+    {
+
+    }
+}
