e0597146e5947f08cbaa272513394e0560883eda
7kQon <kanggakky1214@gmail.com>
2019-04-05 21:59:09 +0900

    feat - 네트워크 적용 2

--- Assets/Scripts/QueueHandler.cs
+++ Assets/Scripts/QueueHandler.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using SocketIO;
+using System;
+using UnityEngine.SceneManagement;
 
 public class QueueHandler : MonoBehaviour
 {
@@ -25,7 +27,45 @@ public class QueueHandler : MonoBehaviour
 
     public void soloMatched(SocketIOEvent e)
     {
+        //SceneManager.LoadScene("SampleScene");
 
+        JSONObject data = e.data;
+
+        int gameRoomIndex;
+        try
+        {
+            gameRoomIndex = (int)data[0].n;
+        }
+        catch (Exception err) { };
+
+        int position;
+        try
+        {
+            position = (int)data[1].n;
+        }
+        catch (Exception err) { };
+
+        JSONObject userListJSON;
+        try
+        {
+            userListJSON = data[2];
+
+            GameObject.Find("UserData").GetComponent<UserData>().clearUserData();
+            int i = 0;
+            foreach (JSONObject userData in userListJSON.list)
+            {
+                Debug.Log("Queue Handler log" + ++i);
+                User player = new User();
+                player.Num = (int)userData[0].n;
+                player.Id = userData[1].str;
+                GameObject.Find("UserData").GetComponent<UserData>().addUser(player);
+            }
+
+            SceneManager.LoadScene("SampleScene");
+        }
+        catch (Exception err) { }
+
+        
     }
 
     public void soloRoomFull(SocketIOEvent e)
