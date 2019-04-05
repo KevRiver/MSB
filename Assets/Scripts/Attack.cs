898e8ca7678db34c85e5bac742f5a31bc1390970
moonjuhan <codemike@naver.com>
2019-04-05 21:51:46 +0900

    feat - 히트마커 사운드 적용

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
+    public GameObject hitmarkerSound;
+    public GameObject hitmarker;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
         {
             Debug.Log("Hit !");
+            hitmarkerSound.GetComponent<AudioSource>().Play();
+            /* 히트마커 임시 삭제
+            Vector3 playerPos = this.gameObject.transform.position;
+            Instantiate(hitmarker, playerPos, Quaternion.identity);
+            */          
+
+
         }
     }
 }
