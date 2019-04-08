155a97c9e8ab40ffedd874940084353cf40df81c
moonjuhan <codemike@naver.com>
2019-04-08 13:03:04 +0900

    Revert "Revert "feat - Gon MJ 프로젝트 합치기""
    This reverts commit 368ca5080e7949bdecffc63e4d7bb4534d09e7da.

---
+++ Assets/Scripts/User.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class User
+{
+    private int num;
+    private string id;
+    private string name;
+    private int rank;
+    private int blocked;
+    private int money;
+    private int gold;
+    private int guild;
+
+    public int Num
+    {
+        get
+        {
+            return num;
+        }
+
+        set
+        {
+            num = value;
+        }
+    }
+
+    public string Id
+    {
+        get
+        {
+            return id;
+        }
+
+        set
+        {
+            id = value;
+        }
+    }
+
+    public string Name
+    {
+        get
+        {
+            return name;
+        }
+
+        set
+        {
+            name = value;
+        }
+    }
+
+    public int Rank
+    {
+        get
+        {
+            return rank;
+        }
+
+        set
+        {
+            rank = value;
+        }
+    }
+
+    public int Blocked
+    {
+        get
+        {
+            return blocked;
+        }
+
+        set
+        {
+            blocked = value;
+        }
+    }
+
+    public int Money
+    {
+        get
+        {
+            return money;
+        }
+
+        set
+        {
+            money = value;
+        }
+    }
+
+    public int Gold
+    {
+        get
+        {
+            return gold;
+        }
+
+        set
+        {
+            gold = value;
+        }
+    }
+
+    public int Guild
+    {
+        get
+        {
+            return guild;
+        }
+
+        set
+        {
+            guild = value;
+        }
+    }
+}
---
+++ Assets/Scripts/UserData.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserData : MonoBehaviour
+{
+    ArrayList m_userlist;
+
+    private void Awake()
+    {
+        m_userlist = new ArrayList();
+        Debug.Log("Arraylist m_userlist 가 만들어졌습니다");
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void addUser(User user)
+    {
+        m_userlist.Add(user);
+        Debug.Log(user.Id + "add to UserData");
+    }
+
+    public ArrayList getUserlist()
+    {
+        return m_userlist;
+    }
+
+    public void clearUserData()
+    {
+        m_userlist.Clear();
+    }
+}
