9dcdd23104f0120fe456433c13bbe81ac23e3890
codemike@naver.com <codemike@naver.com>
2019-05-08 18:50:06 +0900

    feat - 캐릭터 셀렉트 스크롤 뷰 구현

---
+++ Assets/Scripts/Lobby/CharacterSelectUI.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelectUI : MonoBehaviour
+{
+    string gName;
+    int panelID;
+    float vel = 50;
+    Transform panelTrans;
+    GameObject centerPanel;
+    GameObject contentView;
+
+    GameObject textobject;
+    Text t1;
+    string t2;
+
+    float scaleNum;
+    // Start is called before the first frame update    
+    float contentViewPos_x;
+    void Start()
+    {
+
+
+        panelTrans = transform;
+        gName = gameObject.name;
+        centerPanel = GameObject.Find("CenterPos");
+        contentView = GameObject.Find("Content");
+
+        textobject = GameObject.Find("CText");
+        t1 = textobject.GetComponent<Text>();
+       
+
+        // 처음 컨텐츠 오브젝트의 x 위치 
+        contentViewPos_x = contentView.transform.position.x;
+
+        // 위치에 따라 패널 ID 부여
+        if (panelTrans.localPosition.x % 90 != 0) { 
+            panelID = 0; 
+        }
+        else
+        {
+            panelID = (int)panelTrans.localPosition.x / 90;
+        }
+
+        t2 = "Select ID : " + panelID;
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float distance_center = centerPanel.transform.localPosition.x + contentViewPos_x - panelTrans.position.x;
+
+        if(distance_center != 0)
+        {
+            scaleNum = (44f - Mathf.Abs(distance_center)) / 220f;
+            scaleNum += 1;
+        }
+
+        if (44 > distance_center && distance_center > 0)
+        {
+
+            t1.text = t2;
+            //contentView.GetComponent<Rigidbody2D>().AddForce(Vector2.right * vel);
+            panelTrans.localScale = new Vector3(scaleNum, scaleNum, 1.2f);
+
+        }
+        else if(-44 < distance_center && distance_center <= 0)
+        {
+            t1.text = t2;
+            //contentView.GetComponent<Rigidbody2D>().AddForce(Vector2.left * vel);
+            panelTrans.localScale = new Vector3(scaleNum, scaleNum, 1.2f);
+        }
+        else
+        {
+            panelTrans.localScale = new Vector3(1, 1, 1);
+        }
+
+        //중심점과 거리가 얼마나 멀어졌는지 확인.
+        //pos = panelTrans.localPosition.x - .clipOffset.x;
+        //dist = Mathf.Clamp(Mathf.Abs(pos), 0f, cellWidth);
+        //width값을 조절하여 sprite의 size를 조정. 
+        //mWidget.width = System.Convert.ToInt32(((cellWidth - dist * downScale) / cellWidth) * cellWidth);
+    }
+}
---
+++ Assets/Scripts/Lobby/ContentMoving.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ContentMoving : MonoBehaviour
+{
+    public GameObject centerPos;
+    public GameObject[] skin = new GameObject [6];
+    GameObject t1;
+    int centerNum;
+    int instantiateNum;
+    float pos_x;
+
+    float limitLeftPos_x;
+    float limitRightPos_x;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        pos_x = centerPos.transform.position.x;
+        centerNum = skin.Length / 2;
+        instantiateNum = centerNum;
+
+        skinInstantiat();
+
+        limitLeftPos_x = 90 * centerNum;
+        if(skin.Length % 2 == 1)
+        {
+            limitRightPos_x = -90 * centerNum;
+        }
+        else
+        {
+            limitRightPos_x = -90 * (centerNum - 1);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("lp = " + transform.localPosition.x);
+    }
+
+    private void FixedUpdate()
+    {
+        if(transform.localPosition.x > limitLeftPos_x)
+        {
+            transform.localPosition = new Vector2(limitLeftPos_x, transform.localPosition.y);
+        }else if (transform.localPosition.x < limitRightPos_x)
+        {
+            transform.localPosition = new Vector2(limitRightPos_x, transform.localPosition.y);
+        }
+    }
+
+    void skinInstantiat()
+    {
+        for (int x = 0; x < skin.Length; x++)
+        {
+            if (x <= centerNum)
+            {
+                t1 = Instantiate(skin[instantiateNum], new Vector3(pos_x, centerPos.transform.position.y, 0), Quaternion.identity);
+                instantiateNum--;
+                pos_x -= 90f;
+                t1.transform.parent = transform;
+                if (x == centerNum)
+                {
+                    pos_x = centerPos.transform.position.x + 90f;
+                    instantiateNum = centerNum + 1;
+                }
+            }
+            else
+            {
+                t1 = Instantiate(skin[instantiateNum], new Vector3(pos_x, centerPos.transform.position.y, 0), Quaternion.identity);
+                instantiateNum++;
+                pos_x += 90f;
+                t1.transform.parent = transform;
+            }
+        }
+    }
+}
