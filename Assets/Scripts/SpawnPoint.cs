c6ea8760d995f6b99831336dc12fddab2a6babd1
7kQon <kanggakky1214@gmail.com>
2019-10-12 17:46:51 +0900

    Add SpawnPoint.cs
    캐릭터 스포너 오브젝트에 붙일 스크립트

---
+++ Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using MoreMountains.CorgiEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    // each SpawnPoint has number var which stores target MSB_Character's user number
+    public int num;
+    public Character.FacingDirections FacingDirection = Character.FacingDirections.Right;
+    
+    public virtual void SpawnPlayer(MSB_Character player)
+    {
+        player.RespawnAt(transform, FacingDirection);
+    }
+}
