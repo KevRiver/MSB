fd42a5026b29e044a0952b03ef2ff50774963a2c
7kQon <kanggakky1214@gmail.com>
2019-10-20 23:21:30 +0900

    Create CharacterGenerator.cs

---
+++ Assets/Scripts/CharacterGenerator.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterGenerator : MonoBehaviour
+{
+    public List<MSB_Character> PlayerPrefabs;
+    public int PrefabNum = 0;
+
+    private void Awake()
+    {
+        InstantiateCharacter(PrefabNum);
+    }
+
+    const int WEAPON_SWORD = 0;
+    const int WEAPON_SHURIKEN = 1;
+    private void InstantiateCharacter(int PrefabNum)
+    {
+        MSB_Character newPlayer = new MSB_Character();       
+        switch (PrefabNum)
+        {
+            case WEAPON_SWORD:
+                newPlayer = (MSB_Character)Instantiate(PlayerPrefabs[WEAPON_SWORD], new Vector3(0, 0, 0), Quaternion.identity);
+                break;
+
+            case WEAPON_SHURIKEN:
+                newPlayer = (MSB_Character)Instantiate(PlayerPrefabs[WEAPON_SHURIKEN], new Vector3(0, 0, 0), Quaternion.identity);
+                break;
+
+            default:
+                Debug.LogWarning("recieved weapon id is not defined");
+                break;
+        }
+
+        newPlayer.gameObject.AddComponent<RCReciever>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
