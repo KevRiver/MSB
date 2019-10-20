b57a84d827f361ce5ba158820a787717fa9bac0e
7kQon <kanggakky1214@gmail.com>
2019-10-20 23:31:28 +0900

    Create RCSender.cs
    RCSender는 로컬플레이어의 캐릭터와 연동되어 MSB_Network에 Request를 하는 오브젝트이다
    
    RCSender는 Game이 시작되면 RequestUserMove 코루틴을 실행한다
    
    만약 캐릭터가 죽으면 RequestUserMove 코루틴을 중지한다
    
    MSB_Character는 OnEnable 시 자신의 UserNum 과 함께 스폰되었다는 이벤트를 날린다 -> RCSender는 스폰된 캐릭터가 로컬일 경우 다시 RequestUserMove 코루틴을 시작한다
    
    반대로 OnDisable 에서 RequestUserMove 코루틴을 중지한다
    
    게임이 종료되면 코루틴을 중지한다

---
+++ Assets/Scripts/RCSender.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoreMountains.CorgiEngine;
+using MoreMountains.Tools;
+using MSBNetwork;
+
+/// <summary>
+/// 로컬 플레이어와 연결되어 동기화 정보를 보내는 오브젝트
+/// </summary>
+public class RCSender : MonoBehaviour, MMEventListener<MMGameEvent>
+{
+    private int _room;
+
+    public MSB_Character _sender;
+    private CorgiController _controller;
+    private Weapon _weapon;
+
+    private string posX;
+    private string posY;
+    private string posZ;
+    private string speedX;
+    private string speedY;
+    private string isFacingRight;
+
+    public void Initialize(MSB_Character sender)
+    {
+        _room = MSB_GameManager.Instance.RoomNum;
+
+        _sender = sender;
+        _controller = _sender.gameObject.GetComponent<CorgiController>();
+
+        Transform weaponAttachment = _sender.transform.GetChild(0);
+        _weapon = weaponAttachment.transform.GetComponentInChildren<Weapon>();
+    }
+
+    IEnumerator RequestUserMove()
+    {
+        while (true)
+        {
+            posX = (_sender.transform.position.x).ToString();
+            posY = (_sender.transform.position.y).ToString();
+            posZ = (_sender.transform.position.z).ToString();
+            speedX = (_controller.Speed.x).ToString();
+            speedY = (_controller.Speed.y).ToString();
+            isFacingRight = _sender.IsFacingRight.ToString();
+
+            string data = _sender.UserNum.ToString() + "," + posX + "," + posY + "," + posZ + "," + speedX + "," + speedY + "," + isFacingRight;
+            NetworkModule.GetInstance().RequestGameUserMove(_room, data);
+            yield return null;
+        }
+    }
+
+    public void OnMMEvent(MMGameEvent eventType)
+    {
+        switch (eventType.EventName)
+        {
+            case "GameStart":
+                StartCoroutine(RequestUserMove());
+                break;
+
+            case "GameOver":
+                StopCoroutine(RequestUserMove());
+                break;
+        }
+    }
+}
