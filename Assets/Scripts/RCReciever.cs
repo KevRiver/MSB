9578ae758be7c00f28c14dacc3289efbc070f76e
7kQon <kanggakky1214@gmail.com>
2019-10-20 23:25:15 +0900

    Update RCReciever.cs
    Start() 에서 NetworkModule 에 등록

--- Assets/Scripts/RCReciever.cs
+++ Assets/Scripts/RCReciever.cs
@@ -5,103 +5,175 @@ using MoreMountains.Tools;
 using MSBNetwork;
 using MoreMountains.CorgiEngine;
 
-namespace MoreMountains.CorgiEngine
+public class RCReciever : MonoBehaviour
 {
-    public struct MSBActionEvent
-    {
-        public int userNumber;
-        public Vector3 shootAngle;
+    public MSB_Character character;
+    public Transform weaponAttachment;
+    public Weapon weapon;
 
-        public MSBActionEvent(int _userNumber,Vector3 _shootAngle)
-        {
-            userNumber = _userNumber;
-            shootAngle = _shootAngle;
-        }
-
-        static MSBActionEvent e;
-        public static void Trigger(int _userNumber, Vector3 _shootAngle)
-        {
-            e.userNumber = _userNumber;
-            e.shootAngle = _shootAngle;
-            MMEventManager.TriggerEvent(e);
-        }
-    }
-}
-
-public class RCReciever : MonoBehaviour,
-                            NetworkModule.OnGameUserMoveListener,
-                            NetworkModule.OnGameUserSyncListener
-
-{
-    MSB_Character character;
-    Transform weaponAttachment;
-    Weapon weapon;
-
-    private int userNum;
-
-    // Sync frequency
-    const float smoothTime = 0.1f;
-
-    // For position synchronizing
-    Vector3 targetPos;
-    float xSpeed;
-    float ySpeed;
-    private bool isGrounded;
-    private bool isFacingRight;
-
-    // Start is called before the first frame update
+    public int userNum;   
+   
     void Start()
     {
+        Debug.Log("RCReciever Start");
         character = GetComponent<MSB_Character>();
         weaponAttachment = character.transform.GetChild(0);
         weapon = weaponAttachment.GetComponentInChildren<Weapon>();
 
+        Debug.Log("RCReciever Initialized");
+
         userNum = character.UserNum;
-        Debug.Log("User number : " + userNum);
+        //Debug.Log("User number : " + userNum);
         //Debug.Log("Weapon name : " + weapon.gameObject.name);
+
+        NetworkModule networkModule = NetworkModule.GetInstance();
+        networkModule.AddOnEventGameUserMove(new OnGameUserMove(this));
+        networkModule.AddOnEventGameUserSync(new OnGameUserSync(this));
+        networkModule.AddOnEventGameEvent(new OnGameEvent(this));
     }
 
-    private void SyncUserPos()
+    private void SyncUserPos(float targetPosX, float targetPosY, float xSpeed, float ySpeed, float smoothTime = 0.1f)
     {
-        float newPosX = Mathf.SmoothDamp(transform.position.x, targetPos.x, ref xSpeed, smoothTime);
-        float newPosY = Mathf.SmoothDamp(transform.position.y, targetPos.y, ref ySpeed, smoothTime);
+        float newPosX = Mathf.SmoothDamp(transform.position.x, targetPosX, ref xSpeed, smoothTime);
+        float newPosY = Mathf.SmoothDamp(transform.position.y, targetPosY, ref ySpeed, smoothTime);
 
         transform.position = new Vector3(newPosX, newPosY);
     }
 
-    char[] delimiterChars = { ',' };
-    public void OnGameUserMove(object _data)
+    private class OnGameUserMove : NetworkModule.OnGameUserMoveListener
     {
-        string[] dataArray = ((string)_data).Split(delimiterChars);
-        int targetNum = int.Parse(dataArray[0]);
+        private RCReciever rc;
+        private int userNum;
+        private static bool lastFacing;
 
-        //  If this is not target object return
-        if (userNum != targetNum)
-            return;
+        // Target number
+        int targetNum;
+        
+        // For position sync
+        float posX;
+        float posY;
+        float posZ;
+        float xSpeed;
+        float ySpeed;
 
-        // Allocates recieved data
-        float _posX = float.Parse(dataArray[1]);
-        float _posY = float.Parse(dataArray[2]);
-        float _posZ = float.Parse(dataArray[3]);
-        targetPos = new Vector3(_posX, _posY, _posZ);
+        // For facing direction sync
+        bool isFacingRight;
 
-        float _xSpeed = float.Parse(dataArray[4]);
-        float _ySpeed = float.Parse(dataArray[5]);
-        xSpeed = _xSpeed;
-        ySpeed = _ySpeed;
+        public OnGameUserMove(RCReciever _rc)
+        {
+            rc = _rc;
+            userNum = rc.userNum;
+            lastFacing = _rc.character.IsFacingRight;
+        }
 
-        bool _isGrounded = bool.Parse(dataArray[6]);
-        isGrounded = _isGrounded;
+        char[] delimiterChars = { ',' };
+        void NetworkModule.OnGameUserMoveListener.OnGameUserMove(object _data)
+        {
+            string[] dataArray = ((string)_data).Split(delimiterChars);
+            targetNum = int.Parse(dataArray[0]);
 
-        bool _isFacingRight = bool.Parse(dataArray[7]);
-        isFacingRight = _isGrounded;
+            //  If this is not target object, return
+            if (userNum != targetNum)
+                return;
 
-        // Sync user position
-        SyncUserPos();
+            // Allocates recieved data
+
+            posX = float.Parse(dataArray[1]);
+            posY = float.Parse(dataArray[2]);
+            posZ = float.Parse(dataArray[3]);
+
+            xSpeed = float.Parse(dataArray[4]);
+            ySpeed = float.Parse(dataArray[5]);
+
+            isFacingRight = bool.Parse(dataArray[6]);
+
+            // Sync User Facing
+            if (lastFacing != isFacingRight)
+            {
+                lastFacing = !lastFacing;
+                rc.character.Flip();
+            }
+
+            // Sync User position
+            rc.SyncUserPos(posX, posY, xSpeed, ySpeed);
+            
+        }
     }
 
-    public void OnGameUserSync(object _data)
+    private class OnGameUserSync : NetworkModule.OnGameUserSyncListener
     {
-        throw new System.NotImplementedException();
+        private RCReciever rc;
+        private int userNum;
+
+        public OnGameUserSync(RCReciever _rc)
+        {
+            rc = _rc;
+            userNum = rc.userNum;
+        }
+
+        void NetworkModule.OnGameUserSyncListener.OnGameUserSync(object _data)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    private class OnGameEvent : NetworkModule.OnGameEventListener
+    {
+        private RCReciever rc;
+        private int userNum;
+
+        public OnGameEvent(RCReciever _rc)
+        {
+            rc = _rc;
+            userNum = rc.userNum;
+        }
+
+        public void OnGameEventDamage(int from, int to, int amount, string option)
+        {
+            if (to != userNum)
+                return;
+
+            throw new System.NotImplementedException();
+        }
+
+        public void OnGameEventHealth(int num, int health)
+        {
+            if (num != userNum)
+                return;
+
+            throw new System.NotImplementedException();
+        }
+
+        public void OnGameEventItem(int type, int num, int action)
+        {
+            if (num != userNum)
+                return;
+
+            throw new System.NotImplementedException();
+        }
+
+        public void OnGameEventKill(int from, int to, string option)
+        {
+            if (to != userNum)
+                return;
+
+            throw new System.NotImplementedException();
+        }
+
+        public void OnGameEventObject(int num, int health)
+        {
+            if (num != userNum)
+                return;
+
+            throw new System.NotImplementedException();
+        }
+
+        public void OnGameEventRespawn(int num, int time)
+        {
+            if (num != userNum)
+                return;
+
+            throw new System.NotImplementedException();
+        }
     }
 }
