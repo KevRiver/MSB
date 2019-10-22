b899e26c3a139776e12ce83f4735456aeec86fb5
7kQon <kanggakky1214@gmail.com>
2019-10-22 13:51:40 +0900

    Create ButtonScripts.cs

---
+++ Assets/Scripts/ButtonScripts.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MSBNetwork;
+
+public class ButtonScripts : MonoBehaviour
+{
+    private NetworkModule networkModule;
+    private LocalUser localUser;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        networkModule = NetworkModule.GetInstance();
+        localUser = LocalUser.Instance;
+    }
+
+    public void CharacterSelect(int index)
+    {
+        localUser.SetWeaponID(index);
+    }
+
+    public void RequestSoloMatch()
+    {
+        networkModule.RequestSoloQueue(localUser.localUserData.userWeapon, localUser.localUserData.userSkin);
+        Debug.Log("Request Solo Queue");
+    }
+}
